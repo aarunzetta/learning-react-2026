a26ae402638d963e377c967f0fe18804ba3f7040
aaron1458 <aaron.bartolome.code@gmail.com>
2026-02-14 16:06:30 +0800

    learning how to pass props into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-export default function TodoList() {
+export default function Profile() {
   return (
-    <ul
-      style={{
-        backgroundColor: "black",
-        color: "pink",
-      }}
-    >
-      <li style={{ color: "blue" }}>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
+    <div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card title="about">
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
+    </div>
+  );
+}
+
+function Card({ children, title }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
   );
 }
