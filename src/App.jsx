38ba0933f67f797700cea8acb374896c376b34d6
aaron1458 <aaron.bartolome.code@gmail.com>
2026-02-16 16:08:30 +0800

    learning conditional rendering and rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-export default function Profile() {
-  return (
-    <div>
-      <Card title="Photo">
-        <img
-          className="avatar"
-          src="https://i.imgur.com/OKS67lhm.jpg"
-          alt="Aklilu Lemma"
-          width={70}
-          height={70}
-        />
-      </Card>
-      <Card title="about">
-        <p>
-          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
-          natural treatment to schistosomiasis.
-        </p>
-      </Card>
-    </div>
-  );
-}
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist'
+];
 
-function Card({ children, title }) {
+export default function App() {
+  const listItems = people.map(person => <li>{person}</li>);
   return (
-    <div className="card">
-      <div className="card-content">
-        <h1>{title}</h1>
-        {children}
-      </div>
-    </div>
+    <>
+     <ul>{listItems}</ul>
+    </>
   );
 }
